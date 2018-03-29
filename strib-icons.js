c0e5fa5d79852292fc8bb9e465de81c410c97635
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-03-29 14:11:41 -0500

    Updates

---
+++ strib-icons.js
@@ -0,0 +1,24 @@
+/**
+ * Star Tribune icons (strib-icons)
+ * Version: 0.0.6
+ * Copyright Star Tribune
+ *
+ * SVG sprites can't come across different domains, protocols or ports,
+ * so this gets it and inlines it.
+ */
+
+(function() {
+  var version = '0.0.6';
+  var location = '//static.startribune.com/assets/libs/strib-icons/' + version + '/strib-icons.sprite.svg';
+
+  // From: https://css-tricks.com/ajaxing-svg-sprite/
+  var ajax = new XMLHttpRequest();
+  ajax.open('GET', location, true);
+  ajax.send();
+  ajax.onload = function() {
+    var div = document.createElement('div');
+    div.style.display = 'none';
+    div.innerHTML = ajax.responseText;
+    document.body.insertBefore(div, document.body.childNodes[0]);
+  };
+}());
